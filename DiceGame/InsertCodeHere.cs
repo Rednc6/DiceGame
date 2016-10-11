b9496f9381afca9fa459c5655aacfab7d9d504df
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2016-10-11 13:02:06 +0200

    Testing upload to GitHub

--- DiceGame/InsertCodeHere.cs
+++ DiceGame/InsertCodeHere.cs
@@ -10,7 +10,10 @@ namespace DiceGame
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
+            Die Roll01 = new DiceGame.Die();
+            Roll01.RollDie();
 
+            Console.WriteLine("{0}", Roll01.GetValue());
 
             // The LAST line of code should be ABOVE this line
         }
